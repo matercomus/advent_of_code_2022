b2da4167c01a371899dfd10ce644e78c81b619ef
matercomus <matek.kedzia@gmail.com>
2022-12-03 23:27:57 +0100

    fixed 2.1 so it uses original letters for player

--- d2/d2.py
+++ d2/d2.py
@@ -8,32 +8,34 @@ with open("d2/input.txt","r") as input:
     rounds = input.read().split('\n')[:-1]
     rounds = [i.split() for i in rounds]
     #replace opponents xyz for abc respectively for comparisons.
-    for round in rounds:
-        round[1] = round[1].replace('X', 'A')
-        round[1] = round[1].replace('Y', 'B')
-        round[1] = round[1].replace('Z', 'C')
+    #for round in rounds:
+    #    round[1] = round[1].replace('X', 'A')
+    #    round[1] = round[1].replace('Y', 'B')
+    #    round[1] = round[1].replace('Z', 'C')
 
     #[rock,paper,scissors] each kills the index below it (negative indexies will start from end)
     n_points = 0 
     def point_counter(round):
         global n_points
-        rules = ['A','B','C']
+        rules_oponent = ['A','B','C']
+        rules_player = ['X','Y','Z']
         opponent = round[0]
         player = round[1]
 
         #tie
-        if opponent == player:
-            n_points += rules.index(player) +1
+        if opponent == rules_oponent[rules_player.index(player)]:
+            n_points += rules_player.index(player) +1
             n_points += 3
 
         #player wins
-        if opponent == rules[rules.index(player)-1]:
-            n_points += rules.index(player) +1
+        if opponent == rules_oponent[rules_player.index(player)-1]:
+            n_points += rules_player.index(player) +1
             n_points += 6
 
         #Opponent wins
-        if player == rules[rules.index(opponent)-1]:
-            n_points += rules.index(player) +1
+        if player == rules_player[rules_oponent.index(opponent)-1]:
+            n_points += rules_player.index(player) +1
+    
     for round in rounds:
         point_counter(round)
 
