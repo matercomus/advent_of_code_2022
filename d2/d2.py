5ba21ee0b98261402d3c9538ab59485971706144
matercomus <matek.kedzia@gmail.com>
2022-12-04 17:31:41 +0100

    chnaged d2 to use point_counter() for both

--- d2/d2.py
+++ d2/d2.py
@@ -1,14 +1,14 @@
-# Split the input into a list of rounds
+# Read the input from the file
 with open("input.txt","r") as input:
     rounds = input.read().split('\n')[:-1]
     rounds = [i.split() for i in rounds]
 
+# Define the rules for the game
+rules_opponent = ['A','B','C']
+rules_player = ['X','Y','Z']
+
 # [rock, paper, scissors] each kills the index below it (negative indexies will start from end)
 def point_counter(round):
-    # Define the rules for the game
-    rules_opponent = ['A','B','C']
-    rules_player = ['X','Y','Z']
-
     # Get the opponent and player moves
     opponent = round[0]
     player = round[1]
@@ -42,10 +42,6 @@ def p1():
     print(f'Total Points: {total_points}')
 
 def p2():
-    # Define the rules for the game
-    rules_opponent = ['A','B','C']
-    rules_player = ['X','Y','Z']
-
     # Initialize the total points to 0
     total_points = 0
 
@@ -73,17 +69,7 @@ def p2():
             player_index -= 3
 
         # Calculate the points earned in this round
-        points = player_index + 1
-        if opponent == rules_opponent[player_index]:
-            # Tie
-            points += 3
-        elif opponent == rules_opponent[player_index-1]:
-            # Player wins
-            points += 6
-        else:
-            # Opponent wins
-            points += 0
-
+        points = point_counter([opponent, rules_player[player_index]])
         total_points += points
 
     print(f'Total Points: {total_points}')
