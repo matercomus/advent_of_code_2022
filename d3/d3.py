ac6539daf4fb2b1ed103ca8341a218e5d86c0798
matercomus <matek.kedzia@gmail.com>
2022-12-05 02:37:47 +0100

    d3 done p1 and error in p2

--- d3/d3.py
+++ d3/d3.py
@@ -1,2 +1,12 @@
 with open('input.txt','r') as input:
-    pass
+    rucksacks = input.read().split('\n')[:-1]
+    #print(rucksacks)
+
+    def divide_into_compartments(rucksack):
+        first_half = rucksack[:len(rucksack)//2]
+        second_half = rucksack[len(rucksack)//2:]
+        return [first_half,second_half]
+
+    def find_duplicates(rucksack):
+        pass
+    print(rucksacks[0],divide_into_compartments(rucksacks[0]))
